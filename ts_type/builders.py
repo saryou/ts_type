fe74c99fc206dc588b7adcfa6de55cc80e065302
saryou <saryou.ssk@gmail.com>
2021-10-31 17:56:39 +0900

    Revert "ability to customize enum member handling"
    This reverts commit 385e691e0f67ba7e98a5c6aedb50c55d2e746722.

--- ts_type/builders.py
+++ ts_type/builders.py
@@ -17,29 +17,13 @@ T = TypeVar('T')
 BuilderT = TypeVar('BuilderT', bound='NodeBuilder')
 
 
-class EnumMemberHandler(Generic[BuilderT]):
-    def member_to_node(self,
-                       member: Enum,
-                       builder: BuilderT) -> nodes.TypeNode:
-        raise NotImplementedError()
-
-
-class UseNameForEnumMember(EnumMemberHandler[BuilderT]):
-    def member_to_node(self,
-                       member: Enum,
-                       builder: BuilderT) -> nodes.TypeNode:
-        return builder.literal_to_node(member.name)
-
-
 class NodeBuilder:
     """NodeBuilder converts python objects to typescript's types."""
 
-    def __init__(self,
-                 enum_member: Optional[EnumMemberHandler] = None) -> None:
+    def __init__(self) -> None:
         self._modules: Dict[str, Any] = {}
         self._definitions: Dict[str, nodes.TypeNode] = {}
         self._stack: List[Tuple[str, str, Any]] = []
-        self._enum_member = enum_member or UseNameForEnumMember()
 
     @property
     def definitions(self) -> Dict[str, Any]:
@@ -129,7 +113,7 @@ class NodeBuilder:
                 return nodes.Number()
             elif issubclass(t, Enum):
                 return self.define_ref_node(t, lambda: nodes.Union(
-                    [self._enum_member.member_to_node(i, self) for i in t]))
+                    [self.literal_to_node(i) for i in t]))
             elif is_dataclass(t):
                 return self.define_ref_node(
                     t,
@@ -182,8 +166,9 @@ class NodeBuilder:
         return nodes.Reference(_id, ref_typevars)
 
     def literal_to_node(self, value: Any) -> nodes.Literal:
-        assert isinstance(value, (int, bool, str))
-        return nodes.Literal(json.dumps(value))
+        literal = value.name if isinstance(value, Enum) else value
+        assert isinstance(literal, (int, bool, str))
+        return nodes.Literal(json.dumps(literal))
 
     @contextmanager
     def _begin_module_context(self, t: Optional[Type]):
@@ -237,6 +222,4 @@ __all__ = [
     'NodeBuilder',
     'NodeCompatibleClass',
     'NodeCompatible',
-    'EnumMemberHandler',
-    'UseNameForEnumMember',
 ]
